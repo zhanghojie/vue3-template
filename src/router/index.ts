ec018306e084e1b630cbcb7a3ad30bc451b11eac
zhanghj <z168163@163.com>
2022-07-22 14:56:41 +0800

    'init'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,21 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
-	{
-		path: '/login',
-		name: 'Login',
-		meta: {
-			title: '登录',
-			keepAlive: true,
-			requireAuth: false,
-		},
-		component: () => import('@/pages/login.vue'),
-	},
-	{
-		path: '/',
-		name: 'Index',
-		meta: {
-			title: '首页',
-			keepAlive: true,
-			requireAuth: false,
-		},
-		component: () => import('@/pages/index.vue'),
-	},
+  {
+    path: '/',
+    name: 'Index',
+    meta: {
+      title: '首页',
+      keepAlive: true,
+      requireAuth: false,
+    },
+    component: () => import('@/pages/index.vue'),
+  },
 ];
 
 const router = createRouter({
-	history: createWebHashHistory(),
-	routes,
+  history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
